9060104c4db01c1e69647006420b780f26d48bbc
nimesh631 <nimeshkayastha067@gmail.com>
2024-12-18 15:43:16 +0545

    Updated ErrorPage styling for better user experience

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Opps It's a dead end</h1>
-      <Link to="/" style={{ color: "blue" }}>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl text-center mt-10 text-red-950 font-semibold ">Opps It's a dead end</h1>
+      <p className="text-lg text-gray-700 m-6">The page you're looking for does not exist.</p>
+      <Link to="/" className="text-blue-700 font-semibold text-lg ">
         Back Home
       </Link>
     </div>
