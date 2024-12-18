aae172e5831a50fbcb0c7f4dfdb00bbaa17ec01a
nimesh631 <nimeshkayastha067@gmail.com>
2024-12-18 12:51:53 +0545

    feat: Create BookNow component for booking process

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import BookNow from "./components/BookNow";
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl">hello</h1>
+      <BookNow />
     </>
   );
 }
